64a96a8355db59ba2607cd03f85b8f851bf75afc
Awaludin AR <me@awaludin.dev>
2026-02-14 12:43:37 +0700

    refactor(utils): strengthen type guard duck-typing checks

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,14 @@
 import { MariadbPool, MariadbPoolConnection } from "./types";
 
 export function isPool(mariadb: unknown): mariadb is MariadbPool {
-  return !!mariadb && typeof mariadb === 'object' && 'getConnection' in mariadb
+  return !!mariadb && typeof mariadb === 'object'
+    && 'getConnection' in mariadb
+    && 'end' in mariadb
 }
 
 export function isPoolConnection(connection: unknown): connection is MariadbPoolConnection {
-  return !!connection && typeof connection === 'object' && 'query' in connection
+  return !!connection && typeof connection === 'object'
+    && 'query' in connection
+    && 'queryStream' in connection
+    && 'release' in connection
 }
